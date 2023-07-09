1a781fb8f911d10a248e05e2186734ed706772fc
limyel <limyel@outlook.com>
2023-07-09 16:49:06 +0800

    抽离样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,17 @@
 <template>
-  <el-row class="min-h-screen bg-sky-500">
-    <el-col class="flex items-center justify-center" :span="16">
+  <el-row class="login-container">
+    <el-col class="left" :span="16">
       <div>
-        <div class="font-bold text-5xl text-light-50 mb-4">浩元</div>
-        <div class="text-light-900 text-sm">浩元商城后台管理系统</div>
+        <div>浩元</div>
+        <div>浩元商城后台管理系统</div>
       </div>
     </el-col>
-    <el-col class="bg-light-50 flex items-center justify-center flex-col" :span="8">
-      <h2 class="font-bold text-3xl">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-500 space-x-2">
-        <span class="h-[1px] w-16 bg-gray-300"></span>
-        <span class="">账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-300"></span>
+    <el-col class="right" :span="8">
+      <h2>欢迎回来</h2>
+      <div>
+        <span class="line"></span>
+        <span>账号密码登录</span>
+        <span class="line"></span>
       </div>
       <el-form class="w-[250px]" :model="form">
         <el-form-item>
@@ -43,8 +43,34 @@ const form = reactive({})
 
 </script>
 
-<style>
-.main-body {
-  min-height: 100vh;
+<style scoped>
+.login-container {
+  @apply min-h-screen bg-sky-500
+}
+
+.login-container .left {
+  @apply flex items-center justify-center;
+}
+
+.login-container .right {
+  @apply bg-light-50 flex items-center justify-center flex-col;
+}
+
+.left>div>div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+.left>div>div:last-child {
+  @apply text-light-900 text-sm;
+}
+
+.right>h2:first-child {
+  @apply font-bold text-3xl;
+}
+.right>div {
+  @apply flex items-center justify-center my-5 text-gray-500 space-x-2;
+}
+
+.line {
+  @apply h-[1px] w-16 bg-gray-300;
 }
 </style>
